953bc8a6bbea4a736704f3af172702053155a2e4
Sanjith Shyam Sundar <sanjith.shyam006@gmail.com>
2026-06-08 17:57:21 +0100

    Initial Commit

---
+++ Mahjong/MahjongApp.swift
@@ -0,0 +1,17 @@
+//
+//  MahjongApp.swift
+//  Mahjong
+//
+//  Created by Sanjith Shyam Sundar  on 08/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct MahjongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
